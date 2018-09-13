7885f7167f4f778b316832b221819e09bf00f0dc
Max Dekker <xamrekked@gmail.com>
2018-09-13 13:56:52 +0200

    Scripts - MineSweeper
    All MineSweeper scripts by Max Dekker

---
+++ Scripts/Element.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour
+{
+    public GameObject retry;
+
+    public bool mine;
+
+    [SerializeField]
+    private Sprite[] emptytextures;
+
+    [SerializeField]
+    private Sprite minetextures;
+
+    void Start()
+    {
+        mine = Random.value < 0.15;
+
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        GridButCooler.elements[x, y] = this;
+    }
+
+    public void loadTexture(int count)
+    {
+        if (mine)
+        {
+            GetComponent<SpriteRenderer>().sprite = minetextures;
+        }
+        else GetComponent<SpriteRenderer>().sprite = emptytextures[count];
+    }
+
+    public bool isCovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "default";
+    }
+
+    void OnMouseUpAsButton()
+    {
+
+        if (mine)
+        {
+            GridButCooler.UncoverMines();
+            return;
+        }
+        else
+        {
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+            loadTexture(GridButCooler.adjacentMines(x, y));
+
+            GridButCooler.floodfill(x, y, new bool[GridButCooler.w, GridButCooler.h]);
+        }
+    }
+}
+
---
+++ Scripts/Restart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{ 
+    [SerializeField]
+    private Color startcolor;
+    [SerializeField]
+    private Renderer renderer;
+
+    void OnMouseEnter()
+    {
+        startcolor = renderer.material.color;
+        renderer.material.color = Color.red;
+    }
+    void OnMouseExit()
+    {
+        renderer.material.color = startcolor;
+    }
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel(0);
+    }
+}
